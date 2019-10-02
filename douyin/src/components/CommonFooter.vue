1eb9c817339bc8f1a004ecce9da3d759a1561550
YSYtaishuaile <2579599917@qq.com>
2019-10-02 17:35:20 +0800

    222

---
+++ douyin/src/components/CommonFooter.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="footer" :style="{background:$store.state.color}">
+        <router-link @click.native="$store.commit('change',{tittle:'电影',color:'red'})" to="/">电影</router-link>
+        <router-link  @click.native="$store.commit('change',{title:'音乐',color:'green'})" to="/music">音乐</router-link>
+        <router-link  @click.native="$store.commit('change',{title:'图书',color:'orange'})" to="/book">图书</router-link>
+        <router-link  @click.native="$store.commit('change',{title:'图片',color:'yellow'})" to="/photo">图片</router-link>
+    </div>
+</template>
+
+<script>
+    import {mapMuations} from 'vuex'
+    export default {
+        data(){
+            return{
+                menu:[
+                    {
+                        title:'电影',
+                        path:'/',
+                        color:'red'
+                    },
+                    {
+                        title:'音乐'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    #footer{
+        height:1rem;
+        line-height: 1rem;
+        background: #f00;
+        position: fixed;
+        bottom: 0px;
+        width:100%;
+        display: flex;
+        a{
+            flex:1;
+            text-align: center;
+            color:#000;
+            &.router-link-exact-active{
+                color:#fff;
+            }
+        }
+    }
+    
+  
+</style>
---
+++ llalalal/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view/>
+         <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>|
+      <router-link to="/movie">movie</router-link>|
+      
+    </div>
+  </div>
+</template>
+<script>
+    import CommonHeader from'./components/CommonHeader';
+    import CommonFooter from'./components/CommonFooter';
+
+    export default {
+        components:{
+          CommonHeader,
+          CommonFooter
+        }  
+    }
+</script>>
+<style lang="scss">
+
+</style>
